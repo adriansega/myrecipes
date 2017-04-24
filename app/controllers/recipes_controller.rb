a1cb2bffc921ccffd8f6b9dfc2455af936fd8d11
Adrian Segura Gavilan <adriansegavi@gmail.com>
2017-04-24 11:09:24 +0000

    implemente delete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,6 +12,20 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
   
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success] = "Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.chef = Chef.first
@@ -21,6 +35,12 @@ class RecipesController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted successfully"
+    redirect_to recipes_path
   end
   
   private
